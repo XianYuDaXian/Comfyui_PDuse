4bc9fea61864564727a259369c4064818e9702a8
7BEII <1272175942@qq.com>
2025-02-09 00:11:44 +0800

    123
    123

--- __init__.py
+++ __init__.py
@@ -21,9 +21,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
                             "json_group_fontsize": "PD_json_group_fontsize" ,
                             "BatchJsonIncremental": "PD_Incremental_JSON", 
                             "PD_RemoveColorWords": "PD_removeword",  # 新添加的节点
-                            "PD_Image_Crop_Location": "PD Image Crop Location",
-                            "PD_ImageConcanate": "PD ImageConcanate", 
-                            "FileName_refixer": "FileName_refixer"
+                            "PD_Image_Crop_Location": "PD_Image Crop Location",
+                            "PD_ImageConcanate": "PD_ImageConcanate", 
+                            "FileName_refixer": "PD_FileName_refixer"
                                }  
 # 节点显示名称映射，定义了节点的显示名称与其内部名称之间的映射关系
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
